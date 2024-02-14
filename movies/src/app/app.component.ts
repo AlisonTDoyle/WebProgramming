e2926b1211bc5bf1025874c8d7d2cda892d1f331
Blackthrn <blackthorngirl26@hotmail.com>
2024-02-14 11:37:24 +0000

    Completed Worksheet 4

--- movies/src/app/app.component.ts
+++ movies/src/app/app.component.ts
@@ -2,23 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MoviesService } from './services/movies.service';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    AddMovieComponent, 
+    ListMoviesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Movie List';
-
-  movies:any[] = [];
-
-  constructor(private movieService:MoviesService) {}
-
-  ngOnInit() {
-    this.movies = this.movieService.getMovies();
-    console.log(this.movies);
-  }
 }
--- movies/src/app/services/movies.service.ts
+++ movies/src/app/services/movies.service.ts
@@ -8,11 +8,15 @@ export class MoviesService {
   constructor() { }
 
   movieList = [
-    { id: 1, title: "The GodFather", year: '1972', director: 'Francis Ford Coppola' },
-    { id: 2, title: "Millers Corssing", year: '1990', director: 'The Coen Brothers' },
-    { id: 3, title: "Dial M for Murder", year: '1972', director: 'Alfred Hitchcock' },
-    { id: 4, title: "Whiplash", year: '2014', director: 'Damien Chanzelle' },
+    { title: "The GodFather", year: '1972', director: 'Francis Ford Coppola' },
+    { title: "Millers Corssing", year: '1990', director: 'The Coen Brothers' },
+    { title: "Dial M for Murder", year: '1972', director: 'Alfred Hitchcock' },
+    { title: "Whiplash", year: '2014', director: 'Damien Chanzelle' },
   ];
+
+  addMovie(movieTitle:string, movieDirector:string, movieYear:string) {
+    this.movieList.push({title:movieTitle, director:movieDirector, year:movieYear});
+  }
 
   getMovies() {
     return this.movieList;
