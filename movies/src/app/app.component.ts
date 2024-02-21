5e4d174bdd332e300f43fd3388be7f3c9a96a19d
Blackthrn <blackthorngirl26@hotmail.com>
2024-02-21 12:05:26 +0000

    Commpleted Worksheet 5

--- movies/src/app/app.component.ts
+++ movies/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { MoviesService } from './services/movies.service';
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { ListMoviesComponent } from './list-movies/list-movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { Movie } from './models/movie.model';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,19 @@ import { ListMoviesComponent } from './list-movies/list-movies.component';
     RouterOutlet, 
     CommonModule, 
     AddMovieComponent, 
-    ListMoviesComponent
+    ListMoviesComponent,
+    MovieDetailComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Movie List';
+
+  public mySelectedMovie!:Movie;
+
+  setSelectedMovie(movie:Movie) {
+    this.mySelectedMovie=movie;
+  }
 }
--- movies/src/app/services/movies.service.ts
+++ movies/src/app/services/movies.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Movie } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MoviesService {
 
   constructor() { }
 
   movieList = [
-    { title: "The GodFather", year: '1972', director: 'Francis Ford Coppola' },
-    { title: "Millers Corssing", year: '1990', director: 'The Coen Brothers' },
-    { title: "Dial M for Murder", year: '1972', director: 'Alfred Hitchcock' },
-    { title: "Whiplash", year: '2014', director: 'Damien Chanzelle' },
+    new Movie("The GodFather",'1972','Francis Ford Coppola'),
+    new Movie("Millers Corssing",'1990','The Coen Brothers'),
+    new Movie("Dial M for Murder",'1972','Alfred Hitchcock'),
+    new Movie("Whiplash",'2014','Damien Chanzelle'),
   ];
 
+  // Might need to change this to new Movie()
   addMovie(movieTitle:string, movieDirector:string, movieYear:string) {
     this.movieList.push({title:movieTitle, director:movieDirector, year:movieYear});
   }
